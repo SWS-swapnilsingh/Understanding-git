a2b00a4b076ce04fed21a360abb9e7d1df7a56bf
sws@swapnil <sswapnilsingh2002@gmail.com>
2023-04-20 13:56:26 +0530

    commiting multiple files along with index2 file

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__, template_folder = "templates")
 
 @app.route('/base/<name>')
 def home(name):
     return "hello from home!"
+
+@app.route('/hello')
+def hello():
+    return "hello world!"
 
 if __name__=="__main__":
     app.run()
